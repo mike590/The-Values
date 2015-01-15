981eb8aef6e0bdbaf74ae1320248a506b147fa6b
mike590 <mike.gherard@gmail.com>
2015-01-15 18:16:16 -0500

    Completed basic sinatra templatey

---
+++ config.ru
@@ -0,0 +1,6 @@
+require 'bundler'
+Bundler.require(:default)
+
+require "./controllers/application_controller.rb"
+
+map('/'){ run ApplicationController}
---
+++ controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < Sinatra::Base
+
+  set :views, File.expand_path("../../public/views", __FILE__)
+  set :public_folder, File.expand_path("../../public", __FILE__)
+
+  get '/' do
+    erb :index
+  end
+
+
+end
